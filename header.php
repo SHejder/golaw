2b8c57ddde9f99b2601a49e482883319b55709de
maksimsuhanov <syhanov.m@yandex.ru>
2019-09-19 13:06:09 +0300

    minor fixes

--- header.php
+++ header.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="../img/logo/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="../img/logo/favicon.ico" type="image/x-icon">
-    <?php wp_head(); ?>
-    <?php if (is_page(12)):?>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDONeJojxMq_7-PIIds7FCrrmUXmF1wNis"></script>
-    <?php endif;?>
-</head>
 <?php
+global $contacts_link;
 $body_classes = 'page-lang-' . wpm_get_language();
 $nav_classes = 'navigation';
 
@@ -43,6 +31,19 @@ if(isset($type)){
     $header = ob_get_clean();
 }
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../img/logo/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../img/logo/favicon.ico" type="image/x-icon">
+    <?php wp_head(); ?>
+    <?php if (get_page_link() === wpm_translate_url($contacts_link)):?>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDONeJojxMq_7-PIIds7FCrrmUXmF1wNis"></script>
+    <?php endif;?>
+</head>
 <body class="<?= $body_classes; ?>">
 <nav class="<?= $nav_classes; ?>">
     <div class="container">
