bd063a26a61902b5a4625353afb31b0a64109c89
maksimsuhanov <syhanov.m@yandex.ru>
2019-11-21 12:39:03 +0300

    add new translations

--- template-parts/bottom/bottom-cookie.php
+++ template-parts/bottom/bottom-cookie.php
@@ -1,8 +1,8 @@
 <div class="coockies-block cok-b">
     <div class="container">
         <div class="coockies__wrap">
-            <p class="coockies__text"><?php trans('We use cookies to improve functionality and performance, to analyze traffic');?><br>
-                <?php trans('and to provide social media features.');?> <a href="<?= get_permalink(364)?>"><?php trans('Cookies Policy')?></a></p>
+            <p class="coockies__text"><?php trans('We use cookies to improve functionality and performance, to analyze traffic');?>.<br>
+                <a href="<?= get_permalink(364)?>"><?php trans('Cookies Policy')?></a></p>
             <button class="coockies__btn cok"><?php trans('Accept Cookies');?></button>
         </div>
     </div>
